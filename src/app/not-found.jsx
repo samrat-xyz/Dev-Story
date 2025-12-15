754d970c9fd55b28392ae30643b0e4ae7cc0e210
Asif-vai <mr3366533@gmail.com>
2025-12-15 13:13:22 +0600

    add link to go root page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 function NotFound404() {
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
-      <h1 className='text-6xl font-semibold'>404</h1>
+      <h1 className='text-8xl font-semibold'>404</h1>
+      <Link className='mt-8 text-xl font-semibold px-8 py-3 border' href='/'>Go Home</Link>
 
     </div>
   )
