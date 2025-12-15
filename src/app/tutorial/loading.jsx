a61465d2fc913a8aeac61e1e3d6a5798c55b6e0e
Asif-vai <mr3366533@gmail.com>
2025-12-15 13:03:28 +0600

    implement loading

---
+++ src/app/tutorial/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h1 className='text-5xl font-bold'>Loading........</h1>
+    </div>
+  )
+}
+
+export default loading
