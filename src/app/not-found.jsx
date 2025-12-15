f8e9f19494a2bc8e984a8686900c0473be62b8dc
Asif-vai <mr3366533@gmail.com>
2025-12-15 13:06:31 +0600

    not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound404() {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center'>
+      <h1 className='text-6xl font-semibold'>404</h1>
+
+    </div>
+  )
+}
+
+export default NotFound404
